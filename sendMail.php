81006894b79adb59d2001879e0ae53dc2d0144a8
hectorgarcia83 <hectorgarcia83@gmail.com>
2015-11-13 00:12:53 -0700

    se agregan cabeceras al envío de mail

--- sendMail.php
+++ sendMail.php
@@ -8,5 +8,9 @@ $body .= "<br><strong>Email: </strong> ".$_POST["mail"];
 $body .= "<br><strong>Teléfono: </strong> ".$_POST["telefono"];
 $body .= "<br><strong>Mensaje: </strong> ".nl2br($_POST["mensaje"]);
 
-mail($to,$subject,$body,"From:noreply@elcampestre.com");
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$cabeceras .= 'From: Campestre <no-reply@campestre.com>' . "\r\n";
+
+mail($to,$subject,$body,$cabeceras);
 ?>
