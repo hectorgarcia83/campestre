9cd89aa42363b3da0e6d3ce76f42e5f827cab71e
Hector Garcia <hectorgarcia83@gmail.com>
2015-11-18 10:13:31 -0700

    Se cambia logo de sección financiamiento

--- includes/clientes.php
+++ includes/clientes.php
@@ -9,24 +9,24 @@
           <div class="testimonials-slider animated hiding" data-animation="fadeInUp">
             <ul class="slides">
               <li>
-                <img src="images/logo.png" alt="">
+                <img src="images/logo_blanco.png" alt="">
                 <p class="client-comment">El lugar que estabas esperando</p>
                 <!--<p class="client-name">Jasmine - Virtual Inc.</p>-->
               </li>
               <li>
-                <img src="images/logo.png" alt="">
+                <img src="images/logo_blanco.png" alt="">
                 <p class="client-comment">Disfruta al máximo tus fines de semana</p>
               </li>
               <li>
-                <img src="images/logo.png" alt="">
+                <img src="images/logo_blanco.png" alt="">
                 <p class="client-comment">Tienes que vivirlo</p>
               </li>
               <li>
-                <img src="images/logo.png" alt="">
+                <img src="images/logo_blanco.png" alt="">
                 <p class="client-comment">Excelente opción para invertir</p>
               </li>
               <li>
-                <img src="images/logo.png" alt="">
+                <img src="images/logo_blanco.png" alt="">
                 <p class="client-comment">La mejor plusvalía</p>
               </li>
             </ul>
